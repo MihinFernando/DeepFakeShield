bbde8c9d6835584a79e5fa6ef01d738bdcf51eb3
Mihin Fernando <mihinfernando23@gmail.com>
2025-07-29 14:46:13 +0530

    changes

---
+++ Backend/db.py
@@ -0,0 +1,8 @@
+from pymongo import MongoClient
+
+client = MongoClient("mongodb://localhost:27017/")
+db = client['deepfake_shield']
+
+# Collections
+users_collection = db['users']
+reports_collection = db['reports']
--- Backend/routes/history.py
+++ Backend/routes/history.py
@@ -1,7 +1,9 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify
+from db import reports_collection
 
-history_bp = Blueprint("history", __name__)
+history_bp = Blueprint('history_bp', __name__)
 
-@history_bp.route("/history", methods=["GET"])
-def get_history():
-    return {"msg": "History route working!"}
+@history_bp.route('/history/<user_id>', methods=['GET'])
+def get_history(user_id):
+    history = list(reports_collection.find({'user_id': user_id}, {'_id': 0}))
+    return jsonify(history)
