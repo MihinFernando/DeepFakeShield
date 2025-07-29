b86315c871bac7802213750cf9fd079218840d32
Vihanga Rodrigo <vihangarodrigo07@gmail.com>
2025-07-29 10:07:14 +0530

    delete

--- Backend - Copy/routes/history.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint
-
-history_bp = Blueprint("history", __name__)
-
-@history_bp.route("/history", methods=["GET"])
-def get_history():
-    return {"msg": "History route working!"}
