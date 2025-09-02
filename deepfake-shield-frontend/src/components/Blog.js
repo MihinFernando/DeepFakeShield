0ead14909e37c9a7588e9a3da272367cfb987618
KawyaNethmi <kawya.jayawickrama12@gmail.com>
2025-09-02 14:07:37 +0530

    updates in blog

--- deepfake-shield-frontend/src/components/Blog.js
+++ deepfake-shield-frontend/src/components/Blog.js
@@ -14,7 +14,7 @@ const Blog = () => {
       author: "Kawya Nethmi",
       category: "Technology",
       readTime: "5 min read",
-      image: "https://images.unsplash.com/photo-1677442135135-416f8aa26a5b?ixlib=rb-4.0.3&auto=format&fit=crop&w=500&q=80"
+      image: "https://images.unsplash.com/photo-1573164713714-d95e436ab8d6?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1000&q=80"
     },
     {
       id: 2,
