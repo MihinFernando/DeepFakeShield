61264ff49fdc7d468db6e8b9ed7f029f4dd2bfb1
Vihanga Rodrigo <vihangarodrigo07@gmail.com>
2025-10-23 13:56:30 +0530

    Revert "Merge branch 'main' of https://github.com/MihinFernando/DeepFakeShield"
    This reverts commit b84bf21efb86488b3ce6424f0b4d0795a5c30545, reversing
    changes made to f3c316cad4d920085bb770a28bee6c63f2da9e6b.

--- DeepFakeReactNative/src/config/firebase.js
+++ DeepFakeReactNative/src/config/firebase.js
@@ -1,10 +1,6 @@
+// Replace with YOUR Firebase web config (same project your backend's service account belongs to)
 import { initializeApp, getApps } from "firebase/app";
-import {
-  getAuth,
-  initializeAuth,
-  getReactNativePersistence,
-} from "firebase/auth";
-import AsyncStorage from "@react-native-async-storage/async-storage";
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCzgWtX8IAy1WIKNbpu9zAYMuylnG1i-6w",
@@ -12,13 +8,8 @@ const firebaseConfig = {
   projectId: "deepfakeshield-604c7",
   storageBucket: "deepfakeshield-604c7.firebasestorage.app",
   messagingSenderId: "1052171112902",
-  appId: "1:1052171112902:web:4f92ee6074e9f8451862c7",
+  appId: "1:1052171112902:web:4f92ee6074e9f8451862c7"
 };
 
 const app = getApps().length ? getApps()[0] : initializeApp(firebaseConfig);
-
-export const auth = initializeAuth(app, {
-  persistence: getReactNativePersistence(AsyncStorage),
-});
-
-export { app };
+export const auth = getAuth(app);
